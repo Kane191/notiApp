f5718d3271a875df9ce5602e7ab64ff1fea039ae
Kane191 <sarakane17@gmail.com>
2023-11-17 16:32:05 +0300

    colors

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import data from './app.json';
 const App = () => {
   console.log(data);
   return (
-    <div className='flex justify-center border border-black'>
-      <div className='border border-black'>
+    <div className='flex justify-center border border-black bg-lightGrayishBlue2'>
+      <div className='border border-black w-1/2 bg-white rounded-xl p-5'>
         {data.map((notification, idx) => {
           return (
             <div key={idx} className='py-2 flex justify-between'>
@@ -14,23 +14,42 @@ const App = () => {
                   src={`/images/${notification.image}`}
                 />
                 <div>
-                  <div className='flex'>
-                    <p>{notification.name}</p>&nbsp;
-                    <p>{notification.action}</p>&nbsp;
-                    <p>{notification.post}</p>&nbsp;
-                    <p>{notification.group}</p>
-                    <br />
+                  <div className='flex items-center'>
+                    <p className='text-veryDarkBlue font-extrabold'>
+                      {notification.name}
+                    </p>
+                    &nbsp;
+                    <p className='text-grayishBlue font-medium'>
+                      {notification.action}
+                    </p>
+                    &nbsp;
+                    <p className='text-grayishBlue font-extrabold'>
+                      {notification.post}
+                    </p>
+                    &nbsp;
+                    <p className='text-darkGrayishBlue font-extrabold'>
+                      {notification.group}
+                    </p>
+                    &nbsp;
+                    {notification.status === 'unread' ? (
+                      <div className='bg-red-500 h-2 w-2 rounded-full' />
+                    ) : null}
                   </div>
-                  <p>{notification.timeLapse}</p>
+                  <p className='text-grayishBlue'>{notification.timeLapse}</p>
                   {notification.message ? (
-                    <div className='p-3 border border-black rounded-md w-1/2 my-2'>
-                      <p>{notification.message}</p>
+                    <div className='p-3 border border-lightGrayishBlue1 rounded-md w-1/2 my-2'>
+                      <p className='text-grayishBlue font-medium'>
+                        {notification.message}
+                      </p>
                     </div>
                   ) : null}
                 </div>
               </div>
               {notification.sentImage ? (
-                <img src={`/images/${notification.sentImage}`} className="h-12"/>
+                <img
+                  src={`/images/${notification.sentImage}`}
+                  className='h-12'
+                />
               ) : null}
             </div>
           );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: 'hsl(0, 0%, 100%)',
+        veryLightGrayishBlue: 'hsl(210, 60%, 98%)',
+        lightGrayishBlue1: 'hsl(211, 68%, 94%)',
+        lightGrayishBlue2: 'hsl(205, 33%, 90%)',
+        grayishBlue: 'hsl(219, 14%, 63%)',
+        darkGrayishBlue: 'hsl(219, 12%, 42%)',
+        veryDarkBlue: 'hsl(224, 21%, 14%)',
+      },
+    },
   },
   plugins: [],
-}
+};
